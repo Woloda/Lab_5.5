d16fa13cc3d75b292934105ba9ef60cd567988f7
Woloda <dorozovecvoloda@gmail.com>
2020-11-05 19:20:37 +0200

    Lab__5.5

---
+++ Lab_5.5/Lab_5.5.cpp
@@ -0,0 +1,44 @@
+﻿// Lab_5.5.cpp
+// Дорожовець Володимир 
+// Лабораторна робота № 5.5
+// Рекурсивні функції.
+// Варіант 0.6 
+#include <iostream>
+#include <Windows.h>
+
+int f(int n, int level, int& depth);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int x;
+	int n;
+	int depth = 0;
+
+	std::cout << "Введіть число n: ";
+	std::cin >> n;
+	std::cout << "\n";
+
+	x = f(n, 1, depth);
+
+	std::cout << "номер людини, що залишилась: " << x << std::endl;
+	std::cout << "кількість повторень: " << depth << std::endl;
+
+	return 0;
+}
+
+int f(int n, int level, int& depth)
+{
+	if (level > depth)
+		depth = level;
+	std::cout << " level = " << level << std::endl;
+	std::cout << "\n";
+	if (n == 1)
+		return 1;
+	if (n % 2)
+		return 2 * f(n * 1 / 2, level + 1, depth) + 1;
+	else
+		return 2 * f(n * 1 / 2, level + 1, depth) - 1;
+}
---
+++ UnitTest1(Lab_5.5)/UnitTest1(Lab_5.5).cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_5.5/Lab_5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1Lab55
+{
+	TEST_CLASS(UnitTest1Lab55)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int depth = 0;
+			int g = f(20,1,depth);
+			Assert::AreEqual(g, 9);
+		}
+	};
+}
